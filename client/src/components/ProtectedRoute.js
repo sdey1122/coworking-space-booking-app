0386363a88e1eff90279146dfa5e71abe5d2e1ac
Raiku <subhankardey1122@gmail.com>
2023-12-23 09:51:21 +0530

    error-handling-backend-frontend

--- client/src/components/ProtectedRoute.js
+++ client/src/components/ProtectedRoute.js
@@ -1,49 +1,55 @@
-import React, { useEffect, useState, useCallback } from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import { message } from "antd";
+import { useDispatch } from "react-redux";
+import { SetUser } from "../redux/usersSlice";
 
 function ProtectedRoute({ children }) {
+  const dispatch = useDispatch();
   const [loading, setLoading] = useState(true);
   const navigate = useNavigate();
 
-  const validateToken = useCallback(async () => {
-    try {
-      const response = await axios.post(
-        "/api/users/get-user-by-id",
-        {},
-        {
-          headers: {
-            Authorization: `Bearer ${localStorage.getItem("token")}`,
-          },
-        }
-      );
-
-      if (response.data.success) {
-        setLoading(false);
-      } else {
-        setLoading(false);
-        localStorage.removeItem("token");
-        message.error(response.data.message);
-        navigate("/login");
-      }
-    } catch (error) {
-      localStorage.removeItem("token");
-      message.error(error.message);
-      setLoading(false);
-      navigate("/login");
-    }
-  }, [navigate]);
-
   useEffect(() => {
-    if (localStorage.getItem("token")) {
-      validateToken();
-    } else {
-      navigate("/login");
-    }
-  }, [navigate, validateToken]);
+    const token = localStorage.getItem("token");
 
-  return <div>{loading ? <div>Loading...</div> : <>{children}</>}</div>;
+    if (!token) {
+      navigate("/login");
+      return;
+    }
+
+    const validateToken = async () => {
+      try {
+        const response = await axios.post(
+          "/api/users/get-user-by-id",
+          {},
+          {
+            headers: { Authorization: `Bearer ${token}` },
+          }
+        );
+
+        if (response.data.success) {
+          dispatch(SetUser(response.data.data));
+        } else {
+          throw new Error(response.data.message);
+        }
+      } catch (error) {
+        localStorage.removeItem("token");
+        message.error(error.message || "Authentication failed");
+        navigate("/login");
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    validateToken();
+  }, [navigate, dispatch]);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  return <>{children}</>;
 }
 
 export default ProtectedRoute;
